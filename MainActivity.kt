a8408e234f01213c0cc068a4591908b7f60d9e77
RAGUNANDAN14 <220701211@rajalakshmi.edu.in>
2025-02-28 14:44:13 +0530

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,34 +1,37 @@
-package com.example.techlounge
+package com.example.temperatureconverter
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val count : TextView = findViewById(R.id.count)
-        val checkin : Button = findViewById(R.id.checkin)
-        val checkout : Button = findViewById(R.id.checkout)
-        var studentCount=0
-        count.text = studentCount.toString()
+        val editTextTemp = findViewById<EditText>(R.id.editTextText)
+        val btnToFahrenheit = findViewById<Button>(R.id.btnToFahrenheit)
+        val btnToCelsius = findViewById<Button>(R.id.btnToCelsius)
+        val textViewResult = findViewById<TextView>(R.id.textView)
 
-        checkin.setOnClickListener{
-            studentCount++
-            count.text=studentCount.toString()
+        btnToFahrenheit.setOnClickListener {
+            val celsius = editTextTemp.text.toString().toDouble()
+            val fahrenheit = (celsius * 9 / 5) + 32
+            textViewResult.text = "Result: $fahrenheit °F"
         }
 
-        checkout.setOnClickListener{
-            if(studentCount>0)
-            {
-                studentCount--
-                count.text=studentCount.toString()
-            }
+        btnToCelsius.setOnClickListener {
+            val fahrenheit = editTextTemp.text.toString().toDouble()
+            val celsius = (fahrenheit - 32) * 5 / 9
+            textViewResult.text = "Result: $celsius °C"
         }
+        }
+
     }
-}
